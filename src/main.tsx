3695bd53501d5eab8c23da952ef46fdcf478bcbe
jjdavenport <jordanjamesdavenport@gmail.com>
2025-10-16 15:18:42 +0100

    add background to html

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root")!;
+const reactRoot = createRoot(rootElement);
+reactRoot.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+rootElement.classList.add("loaded");
