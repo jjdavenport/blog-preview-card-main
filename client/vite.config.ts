c3ec3ea2ae53c97863d1e45e76688e8d071884b1
jjdavenport <jordanjamesdavenport@gmail.com>
2025-10-17 16:59:58 +0100

    update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: true,
+    allowedHosts: ["blog-preview-card-main-ssr.onrender.com"],
   },
 });
